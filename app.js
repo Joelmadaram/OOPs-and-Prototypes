94a293f06406b5bf2037ab2de7ea5e9df15de62c
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-06-04 18:26:07 -0500

    using new keyword with classes

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-function Color () {
-    this.make = make;
-    this.model = model;
-    this.year = year;
+// function Car (make, model, year) {
+//     this.make = make;
+//     this.model = model;
+//     this.year = year;
+//     console.log(this);
+// }
+
+// const car1 = new Car('Toyota', 'Camry', 2015);
+
+class Color {
+   constructor(r,g,b){
+    console.log('INSIDE CONSTRUCTOR');
+    console.log(r,g,b);
+   }
 }
+
+const c1 = new Color(90,100,110)
